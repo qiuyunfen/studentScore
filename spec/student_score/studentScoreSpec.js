d5e2ffc57904cefb53c3b34b2cb716e7094a9f87
qiuyunfen <1150382430@qq.com>
2017-03-31 14:34:05 +0800

    delete findStuScore method

--- spec/student_score/studentScoreSpec.js
+++ spec/student_score/studentScoreSpec.js
@@ -39,15 +39,15 @@ describe('studentScore', function () {
         expect(findcl).toEqual(cl);
     })
 
-    // it('should return null when student not in class', function () {
-    //     let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
-    //     let cl = new Class(320, 320, 1).addStuScore(stu);
-    //     let allcls = [cl];
-    //
-    //     let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
-    //     let findcl = allcls.find(cl => cl.findClass(stu1));
-    //     expect(findcl).toEqual(undefined);
-    // })
+    it('should return null when student not in class', function () {
+        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let cl = new Class(320, 320, 1).addStuScore(stu);
+        let allcls = [cl];
+
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation: 'han', classno: 2, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
+        let findcl = allcls.find(cl => cl.findClass(stu1));
+        expect(findcl).toEqual(undefined);
+    })
 
     it('should reset stuScore info in class', function () {
         let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
@@ -138,12 +138,6 @@ describe('studentScore', function () {
         expect(test).toEqual(result);
     })
 
-    // it('shoule return string wnationhen input wrong format', function () {
-    //     let input = 'we';
-    //     let res = studentScore.resolveStudId(input);
-    //     expect(res).toEqual('请按正确的格式输入要打印的学生的学号：(格式：学号，学号，...)');
-    // })
-
     it('should get class info include query studentno', function () {
         let stuno = 1;
         let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
@@ -151,7 +145,7 @@ describe('studentScore', function () {
         let allcls = [cl];
 
         let findstu = null;
-        findstu = allcls.find(cl => cl.findStuScore(stuno));
+        findstu = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
         expect(findstu).toEqual(cl);
     })
 
@@ -161,7 +155,7 @@ describe('studentScore', function () {
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let findcl = allcls.find(cl => cl.findStuScore(stuno));;
+        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
         let newcl = findcl.updateNewClass([]);
 
         expect(newcl).toEqual(allcls);
@@ -174,8 +168,8 @@ describe('studentScore', function () {
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let findcl = allcls.filter(cl => cl.findStuScore(stuno));;
-        let newcl = findcl[0].updateNewClass([new Class(360, 360, 1).addStuScore(stu1)]);
+        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
+        let newcl = findcl.updateNewClass([new Class(360, 360, 1).addStuScore(stu1)]);
 
         expect(newcl.length).toEqual(1);
         expect(newcl[0].stuScore.length).toEqual(2);
@@ -189,8 +183,8 @@ describe('studentScore', function () {
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let findcl = allcls.filter(cl => cl.findStuScore(stuno));;
-        let newcl = findcl[0].updateNewClass([new Class(360, 360, 2).addStuScore(stu1)]);
+        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
+        let newcl = findcl.updateNewClass([new Class(360, 360, 2).addStuScore(stu1)]);
 
         expect(newcl.length).toEqual(2);
         expect(newcl[0].stuScore.length).toEqual(1);
@@ -203,8 +197,8 @@ describe('studentScore', function () {
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let findcl = allcls.filter(cl => cl.findStuScore(stuno));;
-        let newcl = findcl[0].updateNewClass([new Class(360, 360, 2).addStuScore(stu1)]);
+        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
+        let newcl = findcl.updateNewClass([new Class(360, 360, 2).addStuScore(stu1)]);
         let res = studentScore.printStuScore(newcl);
         expect(res).toEqual('成绩单\n姓名|数学|语文|英语|编程|平均分|总分\n' +
                             '========================\n'+
