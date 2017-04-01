831dda1d1732e768845dab7f2aece7d9abd0746b
qiuyunfen <1150382430@qq.com>
2017-04-01 09:29:45 +0800

    rename stuno to stuNo

--- spec/student_score/studentScoreSpec.js
+++ spec/student_score/studentScoreSpec.js
@@ -7,7 +7,7 @@ describe('studentScore', function () {
     it('should return student object when input has right format', function () {
         var input = 'Tom,1,han,1,math:75,chinese:95,english:80,program:80';
         var student = studentScore.resolveStudentInfo(input);
-        expect(student).toEqual(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 75, chinese: 95, english: 80, program: 80}));
+        expect(student).toEqual(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 75, chinese: 95, english: 80, program: 80}));
     })
 
     it('should return string when input has wrong format', function () {
@@ -17,20 +17,20 @@ describe('studentScore', function () {
     })
 
     it('should return sutScore object', function () {
-        let student = new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 75, chinese: 95, english: 80, program: 80});
+        let student = new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 75, chinese: 95, english: 80, program: 80});
         let score = student.calStuScore(student);
         expect(score).toEqual(new StuScore(student, 330/4, 330));
     })
 
     it('should add student in class', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         expect(cl.stuScore.length).toEqual(1);
     })
 
 
     it('should return class object when student in class', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
@@ -39,35 +39,35 @@ describe('studentScore', function () {
     })
 
     it('should return null when student not in class', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation: 'han', classno: 2, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuNo:2, nation: 'han', classno: 2, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
         let findcl = allcls.find(cl => cl.classId === stu1.student.classno);
         expect(findcl).toEqual(undefined);
     })
 
     it('should reset stuScore info in class', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
         let findcl = allcls.find(cl => cl.classId === stu.student.classno);
 
-        let stu1 = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 90, chinese: 90, english: 90, program: 90}), 90, 360);
+        let stu1 = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 90, chinese: 90, english: 90, program: 90}), 90, 360);
         findcl = stu1.resetStuScore(findcl);
         expect(findcl.stuScore[0]).toEqual(stu1);
     })
 
     it('should add Class when student has new classId', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let allcls = [];
         let finalcl = studentScore.addClass(allcls, stu);
         expect(finalcl.length).toEqual(1);
     })
     
     it('should add stuScore and classId is exist', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
@@ -77,11 +77,11 @@ describe('studentScore', function () {
     })
 
     it('should return average', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuNo:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
         let findcl = allcls.find(cl => cl.classId === stu1.student.classno);
 
         let avarage = findcl.calAverage(stu1);
@@ -89,11 +89,11 @@ describe('studentScore', function () {
     })
 
     it('should return middle when arr length is even', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuNo:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
         let findcl = allcls.find(cl => cl.classId === stu1.student.classno);
 
         let avarage = findcl.calMiddle(stu1);
@@ -101,26 +101,26 @@ describe('studentScore', function () {
     })
 
     it('should return middle when arr length is odd', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
-        let stu2 = new StuScore(new Student({name: 'Hello', stuno: 3, nation: 'han', classno: 1, math: 81, chinese: 81, english: 81, program: 81}), 81, 324);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu2 = new StuScore(new Student({name: 'Hello', stuNo: 3, nation: 'han', classno: 1, math: 81, chinese: 81, english: 81, program: 81}), 81, 324);
         let cl = new Class(320, 320, 1);
         cl.addStuScore(stu);
         cl.addStuScore(stu2);
 
         let allcls = [cl];
 
-        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuNo:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
         let findcl = allcls.find(cl => cl.classId === stu1.student.classno);
         let middle = findcl.calMiddle(stu1);
         expect(middle).toEqual(324);
     })
 
     it('should update class', function () {
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuNo:2, nation: 'han', classno: 1, math: 85, chinese: 85, english: 85, program: 85}), 85, 340);
         let findcl = allcls.find(cl => cl.classId === stu1.student.classno);
 
         let avarage = findcl.calMiddle(stu1);
@@ -139,22 +139,22 @@ describe('studentScore', function () {
 
     it('should get class info include query studentno', function () {
         let stuno = 1;
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
         let findstu = null;
-        findstu = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
+        findstu = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuNo == stuno));
         expect(findstu).toEqual(cl);
     })
 
     it('shoule update new class info when first add', function () {
         let stuno = '1';
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
+        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuNo == stuno));
         let newcl = findcl.updateNewClass([]);
 
         expect(newcl).toEqual(allcls);
@@ -162,12 +162,12 @@ describe('studentScore', function () {
 
     it('shoule update new class info when classinfo isExit', function () {
         let stuno = '1';
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
-        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation:'han', classno: 1, math: 90, chinese: 90, english: 90, program: 90}), 90, 360);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuNo:2, nation:'han', classno: 1, math: 90, chinese: 90, english: 90, program: 90}), 90, 360);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
+        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuNo == stuno));
         let newcl = findcl.updateNewClass([new Class(360, 360, 1).addStuScore(stu1)]);
 
         expect(newcl.length).toEqual(1);
@@ -177,12 +177,12 @@ describe('studentScore', function () {
 
     it('shoule update new class info when classinfo not exit', function () {
         let stuno = '1';
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
-        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation:'han', classno: 1, math: 90, chinese: 90, english: 90, program: 90}), 90, 360);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuNo:2, nation:'han', classno: 1, math: 90, chinese: 90, english: 90, program: 90}), 90, 360);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
+        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuNo == stuno));
         let newcl = findcl.updateNewClass([new Class(360, 360, 2).addStuScore(stu1)]);
 
         expect(newcl.length).toEqual(2);
@@ -191,12 +191,12 @@ describe('studentScore', function () {
     })
     it('should return class string', function () {
         let stuno = '1';
-        let stu = new StuScore(new Student({name: 'Tom', stuno:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
-        let stu1 = new StuScore(new Student({name: 'Jerry', stuno:2, nation:'han', classno: 1, math: 90, chinese: 90, english: 90, program: 90}), 90, 360);
+        let stu = new StuScore(new Student({name: 'Tom', stuNo:1, nation:'han', classno: 1, math: 80, chinese: 80, english: 80, program: 80}), 80, 320);
+        let stu1 = new StuScore(new Student({name: 'Jerry', stuNo:2, nation:'han', classno: 1, math: 90, chinese: 90, english: 90, program: 90}), 90, 360);
         let cl = new Class(320, 320, 1).addStuScore(stu);
         let allcls = [cl];
 
-        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuno == stuno));
+        let findcl = allcls.find(cl => cl.stuScore.filter(stu => stu.student.stuNo == stuno));
         let newcl = findcl.updateNewClass([new Class(360, 360, 2).addStuScore(stu1)]);
         let res = studentScore.printStuScore(newcl);
         expect(res).toEqual('成绩单\n姓名|数学|语文|英语|编程|平均分|总分\n' +
