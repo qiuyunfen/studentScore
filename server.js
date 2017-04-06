1a911a662a94b694f24974865ca7fe4eaa6f0ade
qiuyunfen <1150382430@qq.com>
2017-04-06 15:14:59 +0800

    add indx.html

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 let express = require('express');
 let app = express();
+let path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('index.html');
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './index.html'));
 })
 
-app.listen(3000, () => {
+app.listen(3000, function () {
     console.log('listen...');
+
 })
