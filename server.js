a3be7b0d95059b945e567415ed2a29aa18eee738
qiuyunfen <1150382430@qq.com>
2017-04-06 15:53:32 +0800

    add ajax

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './index.html'));
 })
 
+app.use('/addStudent', function (req, res) {
+    res.sendFile(path.join(__dirname, './addStudent.html'))
+})
+
 app.listen(3000, function () {
     console.log('listen...');
 
